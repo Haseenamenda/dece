eb241fe51291a4e79c8ea7e4be26855ade9223ed
Haseenamenda <41771159+Haseenamenda@users.noreply.github.com>
2018-10-27 10:44:09 +0530

    Update and rename lcm.py to sort.py

--- lcm.py
+++
@@ -1,8 +0,0 @@
-import sys
-(x,y)=map(int,sys.stdin.readline().split())
-temp=min(x,y)
-while(True):
-	if temp%x==0 and temp%y==0:
-		break
-	temp+=1
-print(temp)
---
+++ sort.py
@@ -0,0 +1,5 @@
+s=input()
+l=list(s)
+l.sort()
+c=''.join(l)
+print(c)
